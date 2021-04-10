2e9d620126f0e5978e98a0bc64ed2ec98f3af71a
Wallyson <wallysongs@gmail.com>
2021-04-10 12:00:54 -0300

    Testando primeira classe de dominio

--- src/main/java/io/github/wallysongs/myseller/resources/CategoriaResource.java
+++ src/main/java/io/github/wallysongs/myseller/resources/CategoriaResource.java
@@ -1,15 +1,22 @@
 package io.github.wallysongs.myseller.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.wallysongs.myseller.entity.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
     
     @GetMapping
-    public String findAll() {
-        return "<h1>Categorias</h1>";
+    public List<Categoria> findAll() {
+        Categoria cat1 = new Categoria(1L, "Informática");
+        Categoria cat2 = new Categoria(2L, "Escritório");
+        return Arrays.asList(cat1, cat2);
     }
 }
