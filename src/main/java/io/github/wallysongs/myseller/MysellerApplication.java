ceb10c2e3b2667f2cf4c3239c7526a38842c6522
Wallyson <wallysongs@gmail.com>
2021-04-10 11:24:24 -0300

    Projeto criado

---
+++ src/main/java/io/github/wallysongs/myseller/MysellerApplication.java
@@ -0,0 +1,13 @@
+package io.github.wallysongs.myseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wallysongs/myseller/MysellerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wallysongs.myseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
